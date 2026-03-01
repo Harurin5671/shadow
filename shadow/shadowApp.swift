206d2770b3cbea44ab7316b5c1277b8e7210f214
Frank Santos <77626612+Harurin5671@users.noreply.github.com>
2026-03-01 18:12:56 -0500

    Initial Commit

---
+++ shadow/shadowApp.swift
@@ -0,0 +1,17 @@
+//
+//  shadowApp.swift
+//  shadow
+//
+//  Created by Frank Erick Santos Gonzales on 1/03/26.
+//
+
+import SwiftUI
+
+@main
+struct shadowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ shadowTests/shadowTests.swift
@@ -0,0 +1,17 @@
+//
+//  shadowTests.swift
+//  shadowTests
+//
+//  Created by Frank Erick Santos Gonzales on 1/03/26.
+//
+
+import Testing
+@testable import shadow
+
+struct shadowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
